b4b3127ed022bef4836847a7944f07b76a43d90e
Alvin Cherk <alvin@cherk.org>
2022-06-02 09:46:26 +1000

    Finish hello world

--- tute01/example/HelloWorld.java
+++ tute01/example/HelloWorld.java
@@ -4,5 +4,11 @@ package example;
  * Prints "Hello World" to the console.
  */
 public class HelloWorld {
-
+    public static void main(String[] args) {
+        // Do I need a \n
+        // printf("Hello World\n");
+        System.out.println("Hello World");
+        System.out.print("Hello World\n");
+        System.out.printf("Hello World\n");
+    }
 }
