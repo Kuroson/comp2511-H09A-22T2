90c89f7ff1e7f058dcb30797a846a85168201f0d
Alvin Cherk <alvin@cherk.org>
2022-08-03 17:12:33 +1000

    Initial commit

---
+++ tute10/src/Computer/Computer.java
@@ -0,0 +1,21 @@
+package Computer;
+
+public class Computer implements ComputerComponent {
+
+    private String name;
+    private int memory;
+
+    public Computer(String name, int memory) {
+        this.name = name;
+        this.memory = memory;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+}
---
+++ tute10/src/Computer/ComputerComponent.java
@@ -0,0 +1,4 @@
+package Computer;
+
+interface ComputerComponent {
+}
